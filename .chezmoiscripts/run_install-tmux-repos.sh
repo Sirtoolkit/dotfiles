639ff63d1ac5c2a02a2cb4d4df48209717fb010b
Cesar Leonardo <cesarleonardo537@gmail.com>
2025-08-21 21:23:22 -0600

    Add installation script for tmux repositories and update VS Code settings
    - Introduced a new script, run_install-tmux-repos.sh, to automate the installation of tmux plugins and provide user guidance upon completion.
    - Updated the Dart SDK path in .vscode/settings.json to reflect the correct user directory for improved development setup.
    - Removed outdated files related to the tmux-gruvbox plugin to clean up the repository.
    
    This commit enhances the tmux environment setup and ensures accurate configuration for Dart development.

---
+++ .chezmoiscripts/run_install-tmux-repos.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+PLUGINS_DIR="$HOME/.tmux/plugins"
+
+TPM_DIR="$PLUGINS_DIR/tpm"
+TPM_REPO="https://github.com/tmux-plugins/tpm.git"
+
+GRUVBOX_DIR="$PLUGINS_DIR/tmux-gruvbox"
+GRUVBOX_REPO="git@github.com:egel/tmux-gruvbox.git"
+
+echo "📂 Verificando que el directorio $PLUGINS_DIR exista..."
+mkdir -p "$PLUGINS_DIR"
+
+if [ -d "$TPM_DIR" ]; then
+  echo "👍 TPM ya está instalado. Omitiendo."
+else
+  echo "🚀 Clonando TPM en $TPM_DIR..."
+  git clone "$TPM_REPO" "$TPM_DIR"
+  echo "✅ TPM clonado correctamente."
+fi
+
+if [ -d "$GRUVBOX_DIR" ]; then
+  echo "👍 El tema tmux-gruvbox ya existe. Omitiendo."
+else
+  echo "🚀 Clonando tmux-gruvbox en $GRUVBOX_DIR..."
+  git clone "$GRUVBOX_REPO" "$GRUVBOX_DIR"
+  echo "✅ Tema clonado correctamente."
+fi
+
+echo "\n🎉 ¡Instalación de todos los repositorios completada!"
+echo "👉 Ahora, añade las líneas de los plugins a tu .tmux.conf y recarga la configuración."
