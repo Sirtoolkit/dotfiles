943c3977026fba8d7b7b1115383205054acd4387
Cesar Leonardo <cesarleonardo537@gmail.com>
2025-11-12 10:53:59 -0600

    ✨ (opencode.jsonc): replace flutter-docs configuration with dart 🐛 (profile.sh): fix PATH export for JetBrains Toolbox scripts and remove unused SSH_AUTH_SOCK variable to clean up the environment configuration

--- dot_config/shell/profile.sh
+++ dot_config/shell/profile.sh
@@ -6,15 +6,15 @@ if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/shell/lscolors.sh" ]; then
 	. "${XDG_CONFIG_HOME:-$HOME/.config}/shell/lscolors.sh"
 fi
 
-# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(mise activate zsh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Added by Toolbox App
-export PATH="$PATH:/Users/cesarleonardo/Library/Application Support/JetBrains/Toolbox/scripts"
 export FLUTTER_ROOT=$(mise where flutter)
 
 export ANDROID_HOME="/opt/homebrew/share/android-commandlinetools"
 export ANDROID_AVD_HOME="$HOME/.config/.android/avd"
+
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export PATH=$PATH:$ANDROID_HOME/emulator
@@ -23,8 +23,6 @@ source ~/.orbstack/shell/init.zsh 2>/dev/null || :
 
 # Dart
 export PATH="$HOME/.pub-cache/bin:$PATH"
-
-export SSH_AUTH_SOCK=~/.1password/agent.sock
 
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
