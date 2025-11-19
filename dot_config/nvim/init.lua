4c773717083cd8b3295661cae9d0eb3f623b3709
Cesar Leonardo <cesarleonardo537@gmail.com>
2025-11-19 14:20:42 -0600

    ♻️ (dot_config/nvim): remove outdated configuration files and README to clean up the project structure ✨ (dot_config/nvim): update dot_luarc.json to include diagnostics globals for better linting support in Neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,25 +1,26 @@
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- init.lua
 
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-  if vim.v.shell_error ~= 0 then
-    -- stylua: ignore
-    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-    vim.fn.getchar()
-    vim.cmd.quit()
-  end
+-- 1. Cargar Opciones Base (vim.opt)
+require("config.options")
+
+-- 2. Cargar Atajos Globales (vim.keymap)
+require("config.keymaps")
+require("config.autocmds")
+
+-- 3. Cargar el gestor de plugins (Lazy)
+-- Nota: Lazy cargará los archivos de lua/plugins/
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
--- validate that lazy is available
-if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
-
-require "lazy_setup"
-require "polish"
+require("lazy").setup("plugins")
