ed53f233dcbd2106ba85c6fb0de19627085da4bd
Cesar Leonardo <cesarleonardo537@gmail.com>
2025-12-11 04:41:07 -0600

    ♻️ (dot_neoconf.json): remove obsolete configuration file to clean up project ✨ (terminal.lua): add key mappings for terminal navigation using vim motions ✨ (none-ls.lua): introduce new plugin configuration to disable phpcs in none-ls

--- dot_config/nvim/lua/config/keymaps/terminal.lua
+++ dot_config/nvim/lua/config/keymaps/terminal.lua
@@ -15,3 +15,9 @@ vim.keymap.set(
 )
 vim.keymap.set("n", "<F7>", "<cmd>ToggleTerm<cr>", { desc = "󰆍 Toggle terminal" })
 vim.keymap.set("n", "<Leader>ts", "<cmd>Telescope toggleterm_manager<cr>", { desc = "󰆍 Search Terminals" })
+
+-- Navegación desde terminal usando vim motions
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Mover a ventana izquierda" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Mover a ventana abajo" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Mover a ventana arriba" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Mover a ventana derecha" })
