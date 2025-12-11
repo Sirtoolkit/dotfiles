887a46d3d41d6be89852e657ce45c05e45654a20
Cesar Leonardo <cesarleonardo537@gmail.com>
2025-12-11 04:41:07 -0600

    ♻️ (dot_neoconf.json): remove obsolete configuration file to clean up project ✨ (terminal.lua): add key mappings for terminal navigation using vim motions ✨ (none-ls.lua): introduce new plugin configuration to disable phpcs in none-ls

---
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,12 @@
+-- Desactivar phpcs de none-ls
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    opts.sources = opts.sources or {}
+    opts.sources = vim.tbl_filter(function(source)
+      return source.name ~= "phpcs"
+    end, opts.sources)
+
+    return opts
+  end,
+}
