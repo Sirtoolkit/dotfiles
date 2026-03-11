ba6c8031b1152378b2bb9893f53d415fd3c9fb70
Cesar Leonardo <cesarleonardo537@gmail.com>
2026-03-11 11:14:06 -0600

    feat(theme): add gruvbox-dark theme configuration for Kilo editor feat(lazy.lua): enable gruvbox.nvim plugin with custom options for better appearance chore(tmux.conf): update tmux configuration to integrate nova theme settings for improved aesthetics

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -35,11 +35,34 @@ return {
   { "projekt0n/github-nvim-theme" },
   { "catppuccin/nvim" },
   { "rebelot/kanagawa.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
   {
-    "LazyVim/LazyVim",
+    "ellisonleao/gruvbox.nvim",
     opts = {
-      colorscheme = "gruvbox",
+      terminal_colors = true,
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true,
+      contrast = "hard",
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
     },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
+      vim.cmd("colorscheme gruvbox")
+    end,
   },
 }
