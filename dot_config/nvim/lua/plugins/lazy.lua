6903fd445bb11f892b55ee4832ec92269ea50e60
Cesar Posada <cesarleonardo537@gmail.com>
2026-06-04 01:29:55 -0600

    chore(nvim): disable several LazyVim language and AI extras
    Remove imports for PHP, dotnet, Go, Astro, Vue, and AI
    sidekick/Copilot to slim the plugin set. Comment out the language
    imports so they can be re-enabled later without losing the config.

--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -1,17 +1,18 @@
 return {
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.prisma" },
-  { import = "lazyvim.plugins.extras.lang.php" },
-  { import = "lazyvim.plugins.extras.lang.dotnet" },
   { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.kotlin" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.astro" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.lang.vue" },
+
+  -- { import = "lazyvim.plugins.extras.lang.vue" },
+  -- { import = "lazyvim.plugins.extras.lang.dotnet" },
+  -- { import = "lazyvim.plugins.extras.lang.go" },
+  -- { import = "lazyvim.plugins.extras.lang.astro" },
+  -- { import = "lazyvim.plugins.extras.lang.php" },
 
   { import = "lazyvim.plugins.extras.editor.dial" },
   { import = "lazyvim.plugins.extras.editor.inc-rename" },
@@ -24,17 +25,11 @@ return {
 
   { import = "lazyvim.plugins.extras.dap.core" },
 
-  { import = "lazyvim.plugins.extras.ai.sidekick" },
-  { import = "lazyvim.plugins.extras.ai.copilot" },
-
   { import = "lazyvim.plugins.extras.ui.edgy" },
 
   { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
 
-  { "projekt0n/github-nvim-theme" },
-  { "catppuccin/nvim" },
-  { "rebelot/kanagawa.nvim" },
   {
     "ellisonleao/gruvbox.nvim",
     opts = {
